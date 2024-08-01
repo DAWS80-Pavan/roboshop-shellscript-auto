f97ec66b602ca8d409e0339df718ea2310e3e4f0
pava <pavareddy1030@gmail.com>
2024-08-01 17:18:15 +0530

    adding code dry

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,21 @@
+source comman.sh
+print copy rabbitmq.repo
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+stat $?
+
+print install rabbitmq-server user
+dnf install rabbitmq-server -y &>>$log_file
+stat $?
+
+print start service
+systemctl enable rabbitmq-server &>>$log_file
+systemctl start rabbitmq-server &>>$log_file
+stat $?
+
+print add application user
+rabbitmqctl add_user roboshop roboshop123 &>>$log_file
+stat $?
+
+print set_permissions for roboshop
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
+stat $?
