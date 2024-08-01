ce256c5e823e90a6dfb6db8c944c190aaebea464
pava <pavareddy1030@gmail.com>
2024-08-01 17:33:35 +0530

    adding code dry

--- payment.sh
+++ payment.sh
@@ -1,6 +1,7 @@
 source comman.sh
 component=payment
 app_path=/app
+systemd_setup
 
 print install python3
 dnf install python3 gcc python3-devel -y &>>$log_file
@@ -13,8 +14,8 @@ print pip3 install
 pip3 install -r requirements.txt &>>$log_file
 stat $?
 
-print start service
-systemctl daemon-reload &>>$log_file
-systemctl enable payment &>>$log_file
-systemctl restart payment &>>$log_file
-stat $?
+#print start service
+#systemctl daemon-reload &>>$log_file
+#systemctl enable payment &>>$log_file
+#systemctl restart payment &>>$log_file
+#stat $?
